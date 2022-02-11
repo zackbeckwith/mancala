f6b561c3a7da892bf0e2098e1e96533458dcd93c
zackbeckwith <z.beckwith99@gmail.com>
2022-02-11 12:20:42 -0500

    variables, cers, event listeners

--- js/app.js
+++ js/app.js
@@ -3,15 +3,43 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-
-
-
+let win, turn, winner, p1store, p2store
+let p2side = []
+let p1side = [] 
 /*------------------------ Cached Element References ------------------------*/
-
-
-
+const pitElements = document.querySelectorAll('.pit')
+const p2Store = document.querySelector('#p2Store')
+const p1Store = document.querySelector('#p1Store')
+const howToButton = document.querySelector('#howToButton')
+const resetButton = document.querySelector('#resetButton')
 /*----------------------------- Event Listeners -----------------------------*/
+pitElements.forEach(function(pit){pit.addEventListener("click",handleClick)})
+pitElements.forEach(function(pit){pit.addEventListener("mouseover",handleHover)})
 
+p1Store.addEventListener("mouseover",handleHover)
+p2Store.addEventListener("mouseover",handleHover)
 
-
+howToButton.addEventListener("click",callHowTo)
+resetButton.addEventListener("click",gameReset)
 /*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+
+function handleClick() {
+  console.log("hi")  
+  }
+  
+function handleHover() {
+console.log("yo")  
+}
+
+function callHowTo() {
+  console.log("help")  
+}
+
+function gameReset() {
+  console.log("bye")  
+}
