59848e7617413988136f12bc1ba84b3824673de6
zackbeckwith <z.beckwith99@gmail.com>
2022-02-11 12:45:22 -0500

    add favicon

--- js/app.js
+++ js/app.js
@@ -3,27 +3,35 @@
 
 
 /*-------------------------------- Variables --------------------------------*/
-let win, turn, winner, p1store, p2store
+let win, turn, winner, p1Store, p2Store
 let p2side = []
 let p1side = [] 
 /*------------------------ Cached Element References ------------------------*/
 const pitElements = document.querySelectorAll('.pit')
-const p2Store = document.querySelector('#p2Store')
-const p1Store = document.querySelector('#p1Store')
+const p2StoreEl = document.querySelector('#p2Store')
+const p1StoreEl = document.querySelector('#p1Store')
 const howToButton = document.querySelector('#howToButton')
 const resetButton = document.querySelector('#resetButton')
 /*----------------------------- Event Listeners -----------------------------*/
 pitElements.forEach(function(pit){pit.addEventListener("click",handleClick)})
 pitElements.forEach(function(pit){pit.addEventListener("mouseover",handleHover)})
 
-p1Store.addEventListener("mouseover",handleHover)
-p2Store.addEventListener("mouseover",handleHover)
+p1StoreEl.addEventListener("mouseover",handleHover)
+p2StoreEl.addEventListener("mouseover",handleHover)
 
 howToButton.addEventListener("click",callHowTo)
 resetButton.addEventListener("click",gameReset)
 /*-------------------------------- Functions --------------------------------*/
+init();
 
-
+function init() {
+  p1side = [4,4,4,4,4,4]
+  p2side = [4,4,4,4,4,4]
+  p1Store = 0
+  p2Store = 0
+  turn = 1
+  winner = null
+}
 
 
 
